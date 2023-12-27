a2854552947d6f1f2f09a6d470a54fbd8346e0a4
Antonio Furone <furone.antonio@gmail.com>
2023-12-27 22:55:51 +0100

    Try fine tuned

--- OpenAITest/TryFineTunedModel.py
+++ OpenAITest/TryFineTunedModel.py
@@ -18,4 +18,13 @@ completion = client.chat.completions.create(
     {"role": "user", "content": "Coud you describe Sparkle Bigdata platform ?"}
   ]
 )
-
+print(completion.choices[0].message)
+print("-----")
+completion = client.chat.completions.create(
+  model="gpt-3.5-turbo-0613",
+  messages=[
+    {"role": "system", "content": "You are a chatbot for support operation on Sparkle's Bigdata platform"},
+    {"role": "user", "content": "How is build datalake and datawarehouse in  Sparkle's Bigdata platform?"}
+  ]
+)
+print(completion.choices[0].message)
